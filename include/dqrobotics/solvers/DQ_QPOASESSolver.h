8b7cf0bf1b21d2a36183830e2d76ae2c7cc39d27
Murilo M Marinho <46012516+mmmarinho@users.noreply.github.com>
2025-07-25 17:14:50 +0900

    [DQ_QPOASESSolver.h] Adjusting use of Eigen.

--- include/dqrobotics/solvers/DQ_QPOASESSolver.h
+++ include/dqrobotics/solvers/DQ_QPOASESSolver.h
@@ -25,7 +25,7 @@ Contributors:
 
 #include <dqrobotics/solvers/DQ_QuadraticProgrammingSolver.h>
 
-#include <eigen3/Eigen/Dense>
+#include <Eigen/Dense>
 using namespace Eigen;
 
 #include <qpOASES.hpp>
